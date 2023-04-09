71fd769a0eedf9baa89b46025c5b41486601c496
Dharmanshu Singh <dharmanshu.singh.21cse@bmu.edu.in>
2023-04-09 19:52:26 +0530

    Flutter1

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Awesome App',
+    home: HomePage(),
+  ));
+}
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Awesome App'),
+      ),
+      body: Container(
+        child: Center(child: Text('Hi flutter'))
+      ),
+    );
+  }
+}
